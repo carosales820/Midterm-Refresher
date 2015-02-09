eaacd6fc798563399052a39ba4ab041d732f5d57
carosales820 <cteig@engr-134-05.eastern.ewu.edu>
2015-02-09 14:31:33 -0800

    midterm-refreshes

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -2,3 +2,15 @@
 // REF: http://api.jquery.com/contains-selector
 // REF: http://api.jquery.com/addclass/
 // Extend into an array of explitives
+
+$('.text:contains("doodoo")').addClass('bad');
+//:contains is a jquery method to filter
+
+console.log($('.text:contains("doodoo")').length);
+
+var bad = ['doodoo', 'nerd', '378', ':-)'];
+
+for (var i=0; i < bad.length; i++){
+	console.log(bad[i]);
+
+}
--- 5/js/scripts.js
+++ 5/js/scripts.js
@@ -2,4 +2,26 @@
 // REF: http://codepen.io/manikoth/pen/BymXwR?editors=101
 // REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
+//get date
+var date = new Date();
+console.log(date);
 
+//store date
+var birthday = {
+  month: date.getMonth(),
+  day: date.getUTCDate(),
+  check: date.getMonth() + "/" + date.getUTCDate()
+};
+
+console.log(birthday.month);
+console.log(birthday.day);
+console.log(birthday.check);
+
+
+//check for birthday, = is assignment == is checking
+if (birthday.check == '1/9' {
+
+};
+
+//show birthday
+$('.date').addClass('bday');
